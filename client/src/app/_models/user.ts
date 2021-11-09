f20256289f8379a66284aa40454b1cff416f3879
Mateusz Kopówka <mkopowka@wp.pl>
2021-11-09 21:22:46 +0100

    End of 16c

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -5,4 +5,5 @@ export interface User {
     photoUrl: string;
     knownAs: string;
     Gender: string;
+    roles: string[];
 }
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, retry } from 'rxjs/operators';
 import { User } from 'src/app/_models/user';
 import { environment } from 'src/environments/environment';
 
@@ -39,6 +39,9 @@ export class AccountService {
   }
 
   setCurrentUser(user: User){
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -46,5 +49,9 @@ export class AccountService {
   logout(){
     localStorage.removeItem('user');
     this.currentUserSource.next(null!);
+  }
+
+  getDecodedToken(token: string){
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
