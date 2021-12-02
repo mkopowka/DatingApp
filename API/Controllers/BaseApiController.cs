dde9e91401d0a95946377fa9e932ae4569b9b4aa
Mateusz Kopówka <mkopowka@wp.pl>
2021-12-02 12:59:45 +0100

    Section 19  - pre switch

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -11,7 +11,6 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 
 {
-    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
