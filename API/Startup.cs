5a98e8054c4ad2969b18ac212c9093ce85f07262
Mateusz Kopówka <mkopowka@wp.pl>
2021-12-04 13:22:10 +0100

    end

--- API/Startup.cs
+++ API/Startup.cs
@@ -49,10 +49,6 @@ namespace API
             }));
             services.AddIdentityServices(_config);
             services.AddSignalR();
-            //services.AddSwaggerGen(c =>
-            //{
-                //c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
